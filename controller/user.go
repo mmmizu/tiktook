373a0868967ba2d0ba97ae489cfb687f5aaaf40c
mizu <1249783661@qq.com>
2023-08-15 22:31:41 +0800

    完成了用户注册登录功能(增加了校验和生成token)

--- controller/user.go
+++ controller/user.go
@@ -3,10 +3,13 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"regexp"
 	"strconv"
 	"tiktook/models"
 	"tiktook/service"
+	"tiktook/util"
 	"time"
+	"unicode/utf8"
 )
 
 type UserLoginResponse struct {
@@ -28,9 +31,14 @@ func Register(c *gin.Context) {
 		c.JSON(http.StatusOK, models.Response{StatusCode: 1, StatusMsg: err.Error()})
 		return
 	}
-	token := username + password
+	// 生成token
+	token, err := util.GenerateToken(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, models.Response{StatusCode: 1, StatusMsg: err.Error()})
+		return
+	}
 	c.JSON(http.StatusOK, UserLoginResponse{
-		Response: models.Response{0, "注册成功"},
+		Response: models.Response{StatusCode: 0, StatusMsg: "注册成功"},
 		UserId:   user.Id,
 		Token:    token,
 	})
@@ -38,12 +46,34 @@ func Register(c *gin.Context) {
 func Login(c *gin.Context) {
 	username := c.Query("username")
 	password := c.Query("password")
+	// 校验账号
+	if utf8.RuneCountInString(username) > 16 || utf8.RuneCountInString(username) <= 0 {
+		c.JSON(http.StatusOK, models.Response{
+			StatusCode: 1,
+			StatusMsg:  "username is valid",
+		})
+	}
+	// 校验密码
+	if ok, _ := regexp.MatchString("^[_a-zA-Z0-9]{5,32}$", password); !ok {
+		c.JSON(http.StatusOK, models.Response{
+			StatusCode: 1,
+			StatusMsg:  "password is valid",
+		})
+	}
 	user, err := service.Login(username, password)
 	if err != nil {
 		c.JSON(http.StatusOK, models.Response{StatusCode: 1, StatusMsg: "用户名或密码错误"})
 		return
 	}
-	token := username + password
+	// 获取token
+	token, err := util.GenerateToken(user)
+	if err != nil {
+		c.JSON(http.StatusOK, models.Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+		return
+	}
 	c.JSON(http.StatusOK, UserLoginResponse{
 		Response: models.Response{StatusCode: 0, StatusMsg: "OK"},
 		UserId:   user.Id,
---
+++ util/jwt.go
@@ -0,0 +1,59 @@
+package util
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v4"
+	"tiktook/models"
+	"time"
+)
+
+type Claims struct {
+	UserID   int64
+	Username string
+	jwt.RegisteredClaims
+}
+
+var mySecret = []byte("titkook")
+
+func GenerateToken(user *models.User) (string, error) {
+	claims := Claims{
+		UserID:   user.Id,
+		Username: user.Name,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(3 * time.Hour * time.Duration(1))),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(mySecret)
+	return token, err
+}
+
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return mySecret, nil
+		})
+	if err != nil {
+		var ve *jwt.ValidationError
+		if errors.As(err, &ve) {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, errors.New("that's not even a token")
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				return nil, errors.New("token is expired")
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, errors.New("token not active yet")
+			} else {
+				return nil, errors.New("couldn't handle this token")
+			}
+		}
+	}
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, errors.New("couldn't handle this token")
+}
